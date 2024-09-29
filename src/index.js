e2b54445244d7e62c1f20abdf4c97f137baaa5f6
Eduardo Berlatto <eduardoberlatto@gmail.com>
2024-09-29 13:18:04 -0300

    feat(components): adiciona novos componentes React

--- src/index.js
+++ src/index.js
@@ -10,6 +10,26 @@ const App = () => {
                 <div className='col-12'> 
                     <h1 className='text-center'>Escolha um personagem para conversar:</h1>
                 </div>
+                <div className='row'>
+                    <div className='col-12 col-xl-3 col-lg-6 mt-3 mb-3'>
+                        <div className='border p-4' style={{ minHeight: '150px' }}>Toretto</div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-12 col-xl-3 col-lg-6 mb-3'>
+                        <div className='border p-4' style={{ minHeight: '150px' }}>Tony Stark</div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-12 col-xl-3 col-lg-6 mb-3'>
+                        <div className='border p-4' style={{ minHeight: '150px' }}>Katniss</div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-12 col-xl-3 col-lg-6 mb-3'>
+                        <div className='border p-4' style={{ minHeight: '150px' }}>Spider Man</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
