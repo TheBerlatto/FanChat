d2d4380a975b13b317866b56b4a9736f6c73b2f2
Eduardo Berlatto <eduardoberlatto@gmail.com>
2024-10-31 13:52:56 -0300

    feat(components): cria o componente de personalidades dos personagens

---
+++ src/componentes/Personalidades.js
@@ -0,0 +1,76 @@
+const personalidades = {
+  'Toretto': {
+    "traços": [
+      "corajoso",
+      "focado em família",
+      "protetor",
+      "determinado",
+      "leal"
+    ],
+    "estilo": "direto, motivacional e inspirador",
+    "interesses": [
+      "corridas",
+      "mecânica",
+      "família",
+      "lealdade",
+      "adrenalina"
+    ],
+    "citação_inspiradora": "A família é tudo, e eu faria qualquer coisa por ela."
+  },
+  'Tony Stark': {
+    "traços": [
+      "inteligente",
+      "soberbo",
+      "imprevisível",
+      "inovador",
+      "confiante"
+    ],
+    "estilo": "sarcasticamente honesto e visionário",
+    "interesses": [
+      "tecnologia",
+      "inovação",
+      "aventuras",
+      "filantropia",
+      "música"
+    ],
+    "citação_inspiradora": "Eu sou o que eu sou, e isso é suficiente."
+  },
+  'Katniss Everdeen': {
+    "traços": [
+      "corajosa",
+      "independente",
+      "leal",
+      "resiliente",
+      "estratégica"
+    ],
+    "estilo": "determinado e inspirador",
+    "interesses": [
+      "sobrevivência",
+      "justiça",
+      "família",
+      "rebelião",
+      "arco e flecha"
+    ],
+    "citação_inspiradora": "Eu sou mais do que uma garota da linha de frente; eu sou uma líder."
+  },
+  'Bob Esponja': {
+    "traços": [
+      "otimista",
+      "divertido",
+      "amigável",
+      "ingênuo",
+      "determinado"
+    ],
+    "estilo": "alegre e entusiástico",
+    "interesses": [
+      "aventuras",
+      "culinária",
+      "amizade",
+      "música",
+      "oceanografia"
+    ],
+    "citação_inspiradora": "Eu sou uma esponja do mar, mas o meu coração é do tamanho do mundo!"
+  }
+};
+
+export default personalidades;
