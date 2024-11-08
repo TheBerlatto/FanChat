ed011e24997b2ca2ccde402d0d0c0afb4e552d1d
Eduardo Berlatto <eduardoberlatto@gmail.com>
2024-11-07 23:03:51 -0300

    chore(project): move App.js para o src

---
+++ src/App.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+//recebe as rotas previamente criadas no logs 
+const logsRouter = require('../db_log/connection/routes/logs.js');
+
+app.use(express.json());
+
+app.get('/hello-world', (req, res) => {
+    res.send('Hello World, eu estou aqui!');
+  });
+
+//ao utilizar a rota /api será chamado as rotas que estão no arquivo logs que contem a rota que faz a requisição ao banco
+app.use('/api', logsRouter);
+
+
+  // Iniciando o servidor
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
